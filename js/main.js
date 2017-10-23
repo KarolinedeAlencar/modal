093dd7296712a146236f1ab14e7d6005d612c314
KarolinedeAlencar <karolinedalencar@gmail.com>
2017-10-23 02:49:34 -0200

    add interactions to modal

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ const finishBtn = document.querySelector('[data-js="finishBtn"]');
 // Steps
 const firstStep = document.querySelector('[data-js="firstStep"]');
 const secondStep = document.querySelector('[data-js="secondStep"]');
-// const finalStep = document.querySelector('[data-js="finalStep"]');
+const finalStep = document.querySelector('[data-js="finalStep"]');
 
 // Breadcrumbs
 const firstBreadcrumb = document.querySelector('[data-js="firstBreadcrumb"]');
 const secondBreadcrumb = document.querySelector('[data-js="secondBreadcrumb"]');
+
+// Form inputs
+// const inputs = Array.from(document.querySelectorAll('.form__field'));
+const dateMonth = document.querySelector('[data-js="dateMonth"]');
+const dateYear = document.querySelector('[data-js="dateYear"]');
+const dateFieldset = document.querySelector('[data-js="dateFieldset"]');
+
 
 function showPlansData(plans) {
   // Plan Price Container
@@ -60,7 +67,7 @@ function showPlansData(plans) {
         paymentMethodsList.innerHTML = `
           <li class="paymentMethods__item">
             <div class="paymentMethods__icon">
-              <img src="img/icon-creditCard.svg" alt="">
+              <img src="img/icon-credit.svg" alt="">
             </div>
             <span class="paymentMethods__method">${plan.payments[0]}</span>
           </li>
@@ -109,6 +116,85 @@ function showPlansData(plans) {
   });
 }
 
+// function checkAllInputs() {
+//   const isAllOk = inputs.every((input) => {
+//      input.dataset.verified === "true";
+//   });
+//   if(isAllOk) {
+//     finishBtn.disabled = false;
+//   } else {
+//     finishBtn.disabled = true;
+//   }
+//
+// }
+
+function showErrorMessage(element) {
+  return element.classList.add('invalid__field');
+}
+
+function luhnAlgorithim(a, b, c, d, e, ...args) {
+  if (args[0] === '') {
+    return false;
+  }
+
+  for (d = +a[b = a.length - 1], e = 0; b--;) {
+    c = +a[b], d += ++e % 2 ? 2 * c % 10 + (c > 4) : c;
+  }
+  return !(d % 10);
+}
+
+function checkDate() {
+  const date = new Date();
+
+  return dateMonth.value < date.getMonth() && Number(dateYear.value) <= date.getFullYear();
+}
+
+function checkCode() {
+  const securityCode = document.querySelector('[data-js="securityCode"]');
+  return securityCode.value.length > 4 || securityCode.value.length <= 2;
+}
+
+function paymentStep() {
+  const cardNumber = document.querySelector('[data-js="cardNumber"]');
+  const securityCode = document.querySelector('[data-js="securityCode"]');
+  const cardName = document.querySelector('[data-js="cardName"]');
+
+  cardNumber.addEventListener('keyup', function () {
+    if (luhnAlgorithim(this.value) === false) {
+      showErrorMessage(this);
+    } else {
+      this.classList.remove('invalid__field');
+    }
+  });
+
+  dateFieldset.addEventListener('focusout', () => {
+    if (checkDate()) {
+      dateMonth.classList.add('invalid__field');
+      dateYear.classList.add('invalid__field');
+    } else {
+      dateMonth.classList.remove('invalid__field');
+      dateYear.classList.remove('invalid__field');
+    }
+    return false;
+  });
+
+  securityCode.addEventListener('keyup', function () {
+    if (checkCode()) {
+      showErrorMessage(this);
+    } else {
+      this.classList.remove('invalid__field');
+    }
+  });
+
+  cardName.addEventListener('keyup', function () {
+    if (this.value === '') {
+      showErrorMessage(this);
+    } else {
+      this.classList.remove('invalid__field');
+    }
+  });
+}
+
 function nextStep() {
   const stepLabels = Array.from(document.querySelectorAll('[data-js="plansRadio"]'));
   const choicePlanType = document.querySelector('[data-js="choicePlanType"]');
@@ -131,6 +217,8 @@ function nextStep() {
   prevBtn.classList.add('active');
   firstBreadcrumb.classList.remove('breadcrump--active');
   secondBreadcrumb.classList.add('breadcrump--active');
+
+  paymentStep();
 }
 
 function prevStep() {
@@ -145,6 +233,19 @@ function prevStep() {
   firstBreadcrumb.classList.add('breadcrump--active');
   secondBreadcrumb.classList.remove('breadcrump--active');
 }
+
+function showFinalStep() {
+  const headerBreadcrumb = document.querySelector('[data-js="headerBreadcrumb"]');
+  const footer = document.querySelector('[data-js="footer"]');
+
+  headerBreadcrumb.style.display = 'none';
+  footer.style.display = 'none';
+  secondStep.classList.remove('active');
+  finishBtn.classList.remove('active');
+  finalStep.classList.add('active');
+  prevBtn.classList.remove('active');
+}
+
 // Data Fetch
 const plansPromise = fetch('server/data.json');
 plansPromise
@@ -153,3 +254,4 @@ plansPromise
 
 nextBtn.addEventListener('click', nextStep);
 prevBtn.addEventListener('click', prevStep);
+finishBtn.addEventListener('click', showFinalStep);
